c63e9746975433ecc9eb09430690c7b3d14d6154
Cem <cemarguvanli@gmail.com>
2014-11-01 16:57:42 -0700

    change

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-	alert("dolar hatası!")
-});
+var productOne = new Array ("T-shirt", 5.99);
+var productTwo = new Array ("Pants", 5.99);
+
+for (var i = 0; i < productOne; i++){
+	document.write(productOne[i]);
+}
